51090fefd5e501d5712083fcf959f6f654c44b92
Staysia <124922319+Staysia@users.noreply.github.com>
2023-05-19 15:34:41 +0400

    5 lab.py

--- 5 lab.py
+++ 5 lab.py
@@ -7,43 +7,33 @@
 import time
 import matplotlib.pyplot as plt
 
-# Рекурсивная функция
-def F_rec(n):
+def rec(n):
     if n == 1 or n == 2:
         return 1
-    else:
-        return F_rec(n-2)*(n-1) + 2
+    if n > 2:
+        return rec((n-2)*(n-1) + 2)
 
-# Итеративная функция
-def F_iter(n):
-    if n == 1 or n == 2:
-        return 1
-    else:
-        a, b = 1, 2
-        for i in range(2, n+1)
-            c = a*b + 2
-        return c
+def iter(n):
+    r = 1
+    for i in range(2, n+1):
+            r = r * ((n-2)*(n-1) + 2)
+        return r
 
-# Ввод числа n
 n = int(input("Введите число n: "))
 
-# Подсчёт времени выполнения рекурсивно
 start_time = time.time()
 f_rec = F_rec(n)
 end_time = time.time()
 recursive_time = end_time - start_time
 
-# Подсчёт времени выполнения итеративно
 start_time = time.time()
 f_iter = F_iter(n)
 end_time = time.time()
 iterative_time = end_time - start_time
 
-# Вывод
 print("F({}) = {} (рекурсивно в {:.6f} секунд)".format(n, f_rec, recursive_time))
 print("F({}) = {} (итеративно в {:.6f} секунд)".format(n, f_iter, iterative_time))
 
-# График
 plt.plot([n], [recursive_time], 'ro', label='Рекурсивно')
 plt.plot([n], [iterative_time], 'bo', label='Итеративно')
 plt.xlabel('n')
