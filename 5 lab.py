29345a4eac9ca2eb80871d4b855ec8557cc3e2c2
Staysia <124922319+Staysia@users.noreply.github.com>
2023-09-15 20:11:28 +0400

    5 lab.py

--- 5 lab.py
+++ 5 lab.py
@@ -1,8 +1,11 @@
-#Задана рекуррентная функция. Область определения функции – натуральные числа.
-#Написать программу сравнительного вычисления данной функции рекурсивно и итерационно.
-#Определить границы применимости рекурсивного и итерационного подхода.
-#Результаты сравнительного исследования времени вычисления представить в табличной и графической форме.
-#23.F(1) = 1, F(2) = 1, F(n) = F(n-2)*(n-1) + 2, при n > 2
+'''
+Задана рекуррентная функция. Область определения функции – натуральные числа.
+Написать программу сравнительного вычисления данной функции рекурсивно и итерационно.
+Определить границы применимости рекурсивного и итерационного подхода.
+Результаты сравнительного исследования времени вычисления представить в табличной и графической форме.
+23.F(1) = 1, F(2) = 1, F(n) = 'F(n-2)'*(n-1) + 2, при n > 2
+'''
+
 
 import timeit
 import matplotlib.pyplot as plt
@@ -10,13 +13,13 @@ import matplotlib.pyplot as plt
 a = []
 b = []
 
-def recF(n):
+def f_rek(n):
     if n == 1 or n == 2:
         return 1
     elif n > 2:
-        return recF(n-2) * (n-1) + 2
+        return f_rek(n-2) * (n-1) + 2
 
-def iterf(n):
+def f_iter(n):
     if n == 1 or n == 2:
         return 1
     else:
@@ -28,15 +31,55 @@ def iterf(n):
             b = c
         return b
 
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print('№' + ' ' + 'Рекурсивно' +'Итеративно')
-for i in nums:
-    a.append(timeit.timeit(lambda: paf(i), number = 20000))
-    b.append(timeit.timeit(lambda: pav(i), number = 20000))
-    print(i, ' ', a[-1], '|', b[-1])
-plt.xlabel('Числовые значения')
-plt.ylabel('Время поиска')
-plt.plot(nums, a, label='Рекурсивно')
-plt.plot(nums, b, label='Итеративно')
-plt.legend()
-plt.show()
+
+try:
+    n = int(input('Введите натуральное число n: '))
+
+    while n < 1:
+        n = int(input("Введите натуральное число: "))
+
+    start = timeit.default_timer()
+    result = f_rek(n)
+    print("\nРекурсия:", result, "\nВремя:", '{:.7f}'.format(timeit.default_timer() - start))
+
+    start = timeit.default_timer()
+    result = f_iter(n)
+    print("\nИтерация:", result, "\nВремя:", '{:.7f}'.format(timeit.default_timer() - start))
+
+
+    s = int(input("С каким шагом будет формироваться таблица? Введите число: "))
+
+    rek_times = []
+    iter_times = []
+    rek_values = []
+    iter_values = []
+    n_values = list(range(1, n + 1,s))
+
+    for n in n_values:
+        start = timeit.default_timer()
+        rek_values.append(f_rek(n))
+        rek_times.append(timeit.default_timer() - start)
+
+        start = timeit.default_timer()
+        iter_values.append(f_iter(n))
+        iter_times.append(timeit.default_timer() - start)
+
+    t_data = []
+    for i, n in enumerate(n_values):
+        t_data.append([n, '{:.7f}'.format(rek_times[i]),'{:.7f}'.format(iter_times[i]), rek_values[i], iter_values[i]])
+    print('{:<5}|{:<15}|{:<15}|{:<35}|{:<35}'.format('n', 'Время рекурсии','Время итерации','Значение рекурсии','Значение итерации'))
+    for data in t_data:
+        print('{:<5}|{:<15}|{:<15}|{:<35}|{:<35}'.format(data[0], data[1],data[2], data[3],data[4]))
+
+    plt.plot(n_values, iter_times, label='Итерация')
+    plt.plot(n_values, rek_times, label='Рекурсия')
+    plt.xlabel('n')
+    plt.ylabel('Время (с)')
+    plt.title('Сравнение рекурсивного и итерационного подхода')
+    plt.legend()
+    plt.show()
+
+
+except ValueError:
+    print('Перезагрузите программу и введите натуральное число')
+    
