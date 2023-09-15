c637f2c7945824e504006521656ff450a1fdfdf4
Staysia <124922319+Staysia@users.noreply.github.com>
2023-09-15 18:35:42 +0400

    6 lab1.py

--- 6 lab1.py
+++ 6 lab1.py
@@ -1,7 +1,78 @@
-#Вариант 23. Составьте все различные лексемы из букв слова «компьютер» по законам русского языка.
-#1 часть – написать программу в соответствии со своим вариантом задания.
+'''
+Вариант 23. Составьте все различные лексемы из букв слова «компьютер» по законам русского языка.
+1 часть – написать программу в соответствии со своим вариантом задания.
+'''
 
 
-import itertools
-for word in itertools.permutations("компьютер"):
-    print(''.join(word))
+import matplotlib.pyplot as plt
+
+
+word = "компьютер"
+for i in range(len(word)):
+    print(word[i])
+
+for i1 in range(len(word)):
+    for i2 in range(len(word)):
+        print(word[i1] + word[i2])
+
+
+for i1 in range(len(word)):
+    for i2 in range(len(word)):
+        for i3 in range(len(word)):
+            print(word[i1] + word[i2] + word[i3])
+
+
+for i1 in range(len(word)):
+    for i2 in range(len(word)):
+        for i3 in range(len(word)):
+            for i4 in range(len(word)):
+                print(word[i1] + word[i2] + word[i3] + word[i4])
+
+
+for i1 in range(len(word)):
+    for i2 in range(len(word)):
+        for i3 in range(len(word)):
+            for i4 in range(len(word)):
+                for i5 in range(len(word)):
+                    print(word[i1] + word[i2] + word[i3] + word[i4] + word[i5])
+
+
+for i1 in range(len(word)):
+    for i2 in range(len(word)):
+        for i3 in range(len(word)):
+            for i4 in range(len(word)):
+                for i5 in range(len(word)):
+                    for i6 in range(len(word)):
+                        print(word[i1] + word[i2] + word[i3] + word[i4] + word[i5] + word[i6])
+
+
+for i1 in range(len(word)):
+    for i2 in range(len(word)):
+        for i3 in range(len(word)):
+            for i4 in range(len(word)):
+                for i5 in range(len(word)):
+                    for i6 in range(len(word)):
+                        for i7 in range(len(word)):
+                            print(word[i1] + word[i2] + word[i3] + word[i4] + word[i5] + word[i6] + word[i7])
+
+
+for i1 in range(len(word)):
+    for i2 in range(len(word)):
+        for i3 in range(len(word)):
+            for i4 in range(len(word)):
+                for i5 in range(len(word)):
+                    for i6 in range(len(word)):
+                        for i7 in range(len(word)):
+                            for i8 in range(len(word)):
+                                print(word[i1] + word[i2] + word[i3] + word[i4] + word[i5] + word[i6] + word[i7] + word[i8])
+
+for i1 in range(len(word)):
+    for i2 in range(len(word)):
+        for i3 in range(len(word)):
+            for i4 in range(len(word)):
+                for i5 in range(len(word)):
+                    for i6 in range(len(word)):
+                        for i7 in range(len(word)):
+                            for i8 in range(len(word)):
+                                for i9 in range(len(word)):
+                                    print(word[i1] + word[i2] + word[i3] + word[i4] + word[i5] + word[i6] + word[i7] + word[i8] + word[i9])
