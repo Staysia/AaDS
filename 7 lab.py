abf0a3df38ca1135ff2b99f4a4e7cb26b85ef73a
Staysia <124922319+Staysia@users.noreply.github.com>
2023-10-16 17:10:09 +0400

    7 lab.py

--- 7 lab.py
+++ 7 lab.py
@@ -3,7 +3,7 @@
 В программе должны быть реализованы минимум один класс, три атрибута, два метода
 
 Вариант 23. Составьте все различные лексемы из букв слова «компьютер» по законам русского языка.
-2 часть – составленные лексемы начинаются и заканчиваются на согласные буквы, вывести эти лексемы и их количество.
+2 часть – составленные лексемы начинаются и заканчиваются на согласные буквы, вывести их количество.
 '''
 
 
@@ -25,22 +25,25 @@ class Computer:
 
     def slovo_kol_1(self):
         for i in range(len(self.word)):
-            print(self.word[i])
+            if self.word[i] in self.letters:
+                print(self.word[i])
 
     def slovo_kol_2(self):
         for i1 in range(len(self.word)):
             for i2 in range(len(self.word)):
                 if self.word[i1] != self.word[i2]:
-                    print(self.word[i1] + self.word[i2])
-                    isEndsCons(self.word[i1] + self.word[i2])
+                    if self.word[i1] in self.letters and self.word[i2] in self.letters:
+                        print(self.word[i1] + self.word[i2])
+                        isEndsCons(self.word[i1] + self.word[i2])
 
     def slovo_kol_3(self):
         for i1 in range(len(self.word)):
             for i2 in range(len(self.word)):
                 for i3 in range(len(self.word)):
                     if len(set([self.word[i1], self.word[i2], self.word[i3]])) == 3:
-                        print(self.word[i1] + self.word[i2] + self.word[i3])
-                        isEndsCons(self.word[i1] + self.word[i2] + self.word[i3])
+                        if self.word[i1] in self.letters and self.word[i3] in self.letters:
+                            print(self.word[i1] + self.word[i2] + self.word[i3])
+                            isEndsCons(self.word[i1] + self.word[i2] + self.word[i3])
 
     def slovo_kol_4(self):
         for i1 in range(len(self.word)):
@@ -48,8 +51,9 @@ class Computer:
                 for i3 in range(len(self.word)):
                     for i4 in range(len(self.word)):
                         if len(set([self.word[i1], self.word[i2], self.word[i3], self.word[i4]])) == 4:
-                            print(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4])
-                            isEndsCons(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4])
+                            if self.word[i1] in self.letters and self.word[i4] in self.letters:
+                                print(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4])
+                                isEndsCons(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4])
 
     def slovo_kol_5(self):
         for i1 in range(len(self.word)):
@@ -58,8 +62,9 @@ class Computer:
                     for i4 in range(len(self.word)):
                         for i5 in range(len(self.word)):
                             if len(set([self.word[i1], self.word[i2], self.word[i3], self.word[i4], self.word[i5]])) == 5:
-                                print(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5])
-                                isEndsCons(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5])
+                                if self.word[i1] in self.letters and self.word[i5] in self.letters:
+                                    print(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5])
+                                    isEndsCons(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5])
 
     def slovo_kol_6(self):
         for i1 in range(len(self.word)):
@@ -69,8 +74,9 @@ class Computer:
                         for i5 in range(len(self.word)):
                             for i6 in range(len(self.word)):
                                 if len(set([self.word[i1], self.word[i2], self.word[i3], self.word[i4], self.word[i5], self.word[i6]])) == 6:
-                                    print(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5] + self.word[i6])
-                                    isEndsCons(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5] + self.word[i6])
+                                    if self.word[i1] in self.letters and self.word[i6] in self.letters:
+                                        print(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5] + self.word[i6])
+                                        isEndsCons(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5] + self.word[i6])
 
     def slovo_kol_7(self):
         for i1 in range(len(self.word)):
@@ -81,8 +87,9 @@ class Computer:
                             for i6 in range(len(self.word)):
                                 for i7 in range(len(self.word)):
                                     if len(set([self.word[i1], self.word[i2], self.word[i3], self.word[i4], self.word[i5], self.word[i6], self.word[i7]])) == 7:
-                                        print(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5] + self.word[i6] + self.word[i7])
-                                        isEndsCons(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5] + self.word[i6] + self.word[i7])
+                                        if self.word[i1] in self.letters and self.word[i7] in self.letters:
+                                            print(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5] + self.word[i6] + self.word[i7])
+                                            isEndsCons(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5] + self.word[i6] + self.word[i7])
 
     def slovo_kol_8(self):
         for i1 in range(len(self.word)):
@@ -94,8 +101,9 @@ class Computer:
                                 for i7 in range(len(self.word)):
                                     for i8 in range(len(self.word)):
                                         if len(set([self.word[i1], self.word[i2], self.word[i3], self.word[i4], self.word[i5], self.word[i6], self.word[i7], self.word[i8]])) == 8:
-                                            print(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5] + self.word[i6] + self.word[i7] + self.word[i8])
-                                            isEndsCons(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5] + self.word[i6] + self.word[i7] + self.word[i8])
+                                            if self.word[i1] in self.letters and self.word[i8] in self.letters:
+                                                print(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5] + self.word[i6] + self.word[i7] + self.word[i8])
+                                                isEndsCons(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5] + self.word[i6] + self.word[i7] + self.word[i8])
 
     def slovo_kol_9(self):
         for i1 in range(len(self.word)):
@@ -108,8 +116,9 @@ class Computer:
                                     for i8 in range(len(self.word)):
                                         for i9 in range(len(self.word)):
                                             if len(set([self.word[i1], self.word[i2], self.word[i3], self.word[i4], self.word[i5], self.word[i6], self.word[i7], self.word[i8], self.word[i9]])) == 9:
-                                                print(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5] + self.word[i6] + self.word[i7] + self.word[i8] + self.word[i9])
-                                                isEndsCons(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5] + self.word[i6] + self.word[i7] + self.word[i8] + self.word[i9])
+                                                if self.word[i1] in self.letters and self.word[i9] in self.letters:
+                                                    print(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5] + self.word[i6] + self.word[i7] + self.word[i8] + self.word[i9])
+                                                    isEndsCons(self.word[i1] + self.word[i2] + self.word[i3] + self.word[i4] + self.word[i5] + self.word[i6] + self.word[i7] + self.word[i8] + self.word[i9])
 
 
 
