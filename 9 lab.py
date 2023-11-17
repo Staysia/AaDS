cde5e711a9bf83db43ea82a7dc5ea6289538e5a3
Staysia <124922319+Staysia@users.noreply.github.com>
2023-11-17 06:19:44 +0400

    9 lab.py

--- 9 lab.py
+++ 9 lab.py
@@ -1,53 +1,153 @@
 from tkinter import *
 from tkinter import messagebox
 
-window = Tk()
-window.title('Авторизация')
-window.geometry('400x300')
-window.resizable(False, False)
+def game():
+    global game_window
+    game_window = Tk()
+    game_window.title("Игра")
+    game_window.geometry("600x600")
+    game_window.resizable(False, False)
 
-def check():
-    data_login = entry_login.get()
-    data_password = entry_password.get()
-    if data_login == '':
-        messagebox.showinfo('Информационное окно', 'Ошибка! Пустое поле! Введите логин!')
-    if data_password == '':
-        messagebox.showinfo('Информационное окно', 'Ошибка! Пустое поле! Введите пароль!')
-    if data_login != '' and data_password != '':
-        window.destroy()
-        #Игровое окно
-        second_window = Tk()
-        second_window.title("Игра")
-        second_window.geometry("600x600")
-        second_window.resizable(False, False)
-        #Информационное окно
-        root = Toplevel()
-        root.title('Информационное окно')
-        root.geometry('400x300')
-        root.resizable(False, False)
-        root.attributes("-topmost", True)
-        label_success = Label(root, text='Поздравляем! Вы успешно вошли!')
-        label_success.pack(pady=80)
-        def root_destroy():
-            root.destroy()
-        button_start = Button(root, text='Начать игру', command=root_destroy)
-        button_start.pack(pady=20)
-        
-def showpass():
-    if entry_password['show'] == '*':
-        entry_password['show'] = ''
+def check_registration():
+    check_login = entry_username_r.get()
+    check_password = entry_password_r.get()
+    if (check_login == '') or (check_password == '') or ((check_login == '') and (check_password == '')):
+        messagebox.showerror('Информационное окно', 'Ошибка! Заполните пустое поле / пустые поля!')
     else:
-        entry_password['show'] = '*'
+        file = open('login.txt', 'w')
+        file.write(entry_username_r.get() + ' ' + entry_password_r.get() + '\n')
+        file.close()
 
-label_1 = Label(window, text='Введите логин:').pack(pady=20)
-entry_login = Entry(window, width=50)
-entry_login.pack()
+        global inf_window
+        inf_window = Tk()
+        inf_window.title('Информационное окно')
+        inf_window.geometry("350x115")
+        inf_window.resizable(False, False)
 
-label_2 = Label(window, text='Введите пароль:').pack(pady=20)
-entry_password = Entry(window, width=50, show='*')
-entry_password.pack()
-button_1 = Button(window, text='Посмотреть пароль', command=showpass).pack(pady=10)
+        lable_inf_1 = Label(inf_window, text='Поздравляем! Вы успешно прошли регестрацию!')
+        lable_inf_1.pack(ipady=10)
+        lable_inf_2 = Label(inf_window, text='Для начала игры войдите в аккаунт.')
+        lable_inf_2.pack()
 
-button_2 = Button(window, text='Войти', command=check).pack(pady=20)
+        def inf_window_destroy():
+            inf_window.destroy()
 
-window.mainloop()
+        def registration_window_destroy():
+            registration_window.destroy()
+
+        button_inf = Button(inf_window, text='Ок', command=lambda: [inf_window_destroy(), registration_window_destroy(), main_account_screen()])
+        button_inf.pack(pady=10)
+
+def registration():
+    global registration_window
+    registration_window = Tk()
+    registration_window.title('Регестрация')
+    registration_window.geometry("400x300")
+    registration_window.resizable(False, False)
+
+    global username_r
+    global password_r
+    global entry_username_r
+    global entry_password_r
+    username_r = StringVar()
+    password_r = StringVar()
+
+    lable_username_r = Label(registration_window, text='Придумайте логин:')
+    lable_username_r.pack(pady=20)
+    entry_username_r = Entry(registration_window, width=50)
+    entry_username_r.pack()
+
+    def showpass_r():
+        if entry_password_r['show'] == '*':
+            entry_password_r['show'] = ''
+        else:
+            entry_password_r['show'] = '*'
+
+    lable_password_r = Label(registration_window, text='Придумайте пароль:')
+    lable_password_r.pack(pady=20)
+    entry_password_r = Entry(registration_window, width=50, show='*')
+    entry_password_r.pack()
+
+    button_password_r = Button(registration_window, text='Посмотреть пароль', command=showpass_r)
+    button_password_r.pack(pady=10)
+
+    button_register = Button(registration_window, text='Зарегестрироваьтся', command=check_registration)
+    button_register.pack(pady=10)
+
+def check_log_in():
+    check_login_1 = entry_username.get()
+    check_password_1 = entry_password.get()
+    if (check_login_1 == '') or (check_password_1 == '') or ((check_login_1 == '') and (check_password_1 == '')):
+        messagebox.showerror('Информационное окно', 'Ошибка! Заполните пустое поле / пустые поля!')
+    else:
+        file = open('login.txt', 'r')
+        while TRUE:
+            line = file.readline()
+            line = line.split()
+            if len(line) == 0:
+                file.close()
+                messagebox.showerror('Ошибка!', 'Неверный лигин/пароль!')
+            elif line[0] == entry_username.get() and line[1] == entry_password.get():
+                file.close()
+
+                window.destroy()
+
+                window_inf = Tk()
+                window_inf.title('Информационное окно')
+                window_inf.geometry('350x115')
+                window_inf.resizable(False, False)
+
+                lable_inf = Label(window_inf, text='Поздравляем! Вы успешно вошли!')
+                lable_inf.pack(pady=20)
+
+                def window_inf_destroy():
+                    window_inf.destroy()
+
+                button_start = Button(window_inf, text='Начать игру', command=lambda:[window_inf_destroy(), game()])
+                button_start.pack(pady=10)
+
+def main_account_screen():
+    global window
+    window = Tk()
+    window.title('Авторизация')
+    window.geometry('400x350')
+    window.resizable(False, False)
+
+    global username
+    global password
+    global entry_username
+    global entry_password
+    username = StringVar()
+    password = StringVar()
+
+    lable_username = Label(window, text='Введите логин:')
+    lable_username.pack(pady=20)
+    entry_username = Entry(window, width=50)
+    entry_username.pack()
+
+    def showpass():
+        if entry_password['show'] == '*':
+            entry_password['show'] = ''
+        else:
+            entry_password['show'] = '*'
+
+    lable_password = Label(window, text='Введите пароль:')
+    lable_password.pack(pady=20)
+    entry_password = Entry(window, width=50, show='*')
+    entry_password.pack()
+
+    button_password = Button(window, text='Посмотреть пароль', command=showpass)
+    button_password.pack(pady=10)
+
+    button_entry = Button(window, text='Войти', command=check_log_in)
+    button_entry.pack(pady=20)
+
+    def window_destroy():
+        window.destroy()
+
+    button_registration = Button(window, text='Ещё не зарегестрированы? Зарегестрироваться', command=lambda:[window_destroy(), registration()])
+    button_registration.pack(pady=10)
+
+    window.mainloop()
+
+main_account_screen()
