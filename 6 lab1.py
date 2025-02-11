a33e5c7a6e917da20dcf52ba1edb9ae4879d1860
Staysia <124922319+Staysia@users.noreply.github.com>
2025-02-11 13:27:58 +0400

    Update 6 lab1.py

--- 6 lab1.py
+++ 6 lab1.py
@@ -10,20 +10,22 @@ for i in range(len(word)):
 
 for i1 in range(len(word)):
     for i2 in range(len(word)):
-        print(word[i1] + word[i2])
+        if i1 != i2:
+            print(word[i1] + word[i2])
 
 
 for i1 in range(len(word)):
     for i2 in range(len(word)):
         for i3 in range(len(word)):
-            print(word[i1] + word[i2] + word[i3])
-
+            if i1 != i2 and i2 != i3:
+                print(word[i1] + word[i2] + word[i3])
 
 for i1 in range(len(word)):
     for i2 in range(len(word)):
         for i3 in range(len(word)):
             for i4 in range(len(word)):
-                print(word[i1] + word[i2] + word[i3] + word[i4])
+                if i1 != i2 and i2 != i3 and i3 != i4:
+                    print(word[i1] + word[i2] + word[i3] + word[i4])
 
 
 for i1 in range(len(word)):
@@ -31,7 +33,8 @@ for i1 in range(len(word)):
         for i3 in range(len(word)):
             for i4 in range(len(word)):
                 for i5 in range(len(word)):
-                    print(word[i1] + word[i2] + word[i3] + word[i4] + word[i5])
+                    if i1 != i2 and i2 != i3 and i3 != i4 and i4 != i5:
+                        print(word[i1] + word[i2] + word[i3] + word[i4] + word[i5])
 
 
 for i1 in range(len(word)):
@@ -40,7 +43,8 @@ for i1 in range(len(word)):
             for i4 in range(len(word)):
                 for i5 in range(len(word)):
                     for i6 in range(len(word)):
-                        print(word[i1] + word[i2] + word[i3] + word[i4] + word[i5] + word[i6])
+                        if i1 != i2 and i2 != i3 and i3 != i4 and i4 != i5 and i5 != i6:
+                            print(word[i1] + word[i2] + word[i3] + word[i4] + word[i5] + word[i6])
 
 
 for i1 in range(len(word)):
@@ -50,7 +54,8 @@ for i1 in range(len(word)):
                 for i5 in range(len(word)):
                     for i6 in range(len(word)):
                         for i7 in range(len(word)):
-                            print(word[i1] + word[i2] + word[i3] + word[i4] + word[i5] + word[i6] + word[i7])
+                            if i1 != i2 and i2 != i3 and i3 != i4 and i4 != i5 and i5 != i6 and i6 != i7:
+                                print(word[i1] + word[i2] + word[i3] + word[i4] + word[i5] + word[i6] + word[i7])
 
 
 for i1 in range(len(word)):
@@ -61,7 +66,8 @@ for i1 in range(len(word)):
                     for i6 in range(len(word)):
                         for i7 in range(len(word)):
                             for i8 in range(len(word)):
-                                print(word[i1] + word[i2] + word[i3] + word[i4] + word[i5] + word[i6] + word[i7] + word[i8])
+                                if i1 != i2 and i2 != i3 and i3 != i4 and i4 != i5 and i5 != i6 and i6 != i7 and i7 != i8:
+                                    print(word[i1] + word[i2] + word[i3] + word[i4] + word[i5] + word[i6] + word[i7] + word[i8])
 
 for i1 in range(len(word)):
     for i2 in range(len(word)):
@@ -72,4 +78,5 @@ for i1 in range(len(word)):
                         for i7 in range(len(word)):
                             for i8 in range(len(word)):
                                 for i9 in range(len(word)):
-                                    print(word[i1] + word[i2] + word[i3] + word[i4] + word[i5] + word[i6] + word[i7] + word[i8] + word[i9])
+                                    if i1 != i2 and i2 != i3 and i3 != i4 and i4 != i5 and i5 != i6 and i6 != i7 and i7 != i8 and i8 != i9:
+                                        print(word[i1] + word[i2] + word[i3] + word[i4] + word[i5] + word[i6] + word[i7] + word[i8] + word[i9])
